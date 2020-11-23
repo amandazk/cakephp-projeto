eb85920fdc86f3cbf1c37d73b7b8cdce582ad818
amandazk <zacharukaz@gmail.com>
2020-11-23 11:23:04 -0300

    Começando a tabela de listagem de produtos

--- templates/Produtos/index.php
+++ templates/Produtos/index.php
@@ -1,1 +1,18 @@
-<h1><?= $msg; ?></h1>
+<table class="table">
+    <thead>
+        <tr>
+            <th>Id</th>
+            <th>Nome</th>
+            <th>Preço</th>
+            <th>Descrição</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <th>1</th>
+            <th>HD 20gb</th>
+            <th>59.99</th>
+            <th>HD de 20gb da HP</th>
+        </tr>
+    </tbody>
+</table>
