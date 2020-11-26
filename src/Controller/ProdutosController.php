c77ed2c585d97f1f6aa426ecf8c87a79ba9aad91
amandazk <zacharukaz@gmail.com>
2020-11-26 18:10:37 -0300

    Alterações no controller

--- src/Controller/ProdutosController.php
+++ src/Controller/ProdutosController.php
@@ -7,8 +7,8 @@ use Cake\ORM\TableRegistry;
 class ProdutosController extends AppController {
 
     public function index() {
+        //ele busca automaticamente uma view com o nome da função
         $produtosTable = TableRegistry::getTableLocator()->get('Produtos');
-
         $produtos = $produtosTable->find('all');
 
         // pra mandar uma variável pra visualização
@@ -23,16 +23,23 @@ class ProdutosController extends AppController {
 
     public function salva() {
         $produtosTable = TableRegistry::getTableLocator()->get('Produtos');
-        $produto = $produtosTable -> newEmptyEntity($this->request->getData());
-        $msg = "";
+        $dados = $this->request->getData();
+        $produto = $produtosTable -> newEmptyEntity($dados);
         if ($produtosTable->save($produto)) {
-            $msg = "Produto salvo com sucesso :)";
+            $msg = "Produto salvo com sucesso";
         } else {
             $msg = "Não foi possível salvar o produto";
         }
         // print_r($this->request->getData()); exit;
         //pra disponibilizar a msg pra view (salva.php)
         $this->set('msg', $msg);
+    }
+
+    public function editar($id) {
+        $produtosTable = TableRegistry::getTableLocator()->get('Produtos');
+        $produto = $produtosTable->get($id);
+        $this->set('produto', $produto);
+        $this->render('novo');
     }
 }
 
